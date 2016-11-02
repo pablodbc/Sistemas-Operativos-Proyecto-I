981cbe54a96c7f8f0c7761b977ff04ae76a89e80
pablo <pablodbc30@gmail.com>
2016-11-02 01:21:58 -0400

    Mejora al generador

--- test_case_generator.py
+++ test_case_generator.py
@@ -5,7 +5,7 @@ print(N*N)
 for i in range(N):
 	for j in range(N):
 		print(i,j,((-1)**randint(0,1))*randint(0,100000))
-B = randint(1,N*N)
+B = randint(1,1000)
 print(B)
 for i in range(B):
 	print(randint(0,N*N-1),randint(0,N*N - 1),randint(1,N*N-1),randint(0,100000))
